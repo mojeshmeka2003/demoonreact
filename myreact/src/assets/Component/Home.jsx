09ed273b9ca15d6b0c4d9a8835edb559c8d1d22b
Moses <mojeshmeka6006@gmail.com>
2023-06-17 11:12:08 +0530

    170623

--- myreact/src/assets/Component/Home.jsx
+++ myreact/src/assets/Component/Home.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+import Product from './Product';
 
-export default function Home(){
-    return (
-        <div> 
-            <h1> Home </h1>
-            </div>
-    )
+
+export default function Home() {
+    let [ data, setData] = useState([])
+
+    const fetchData = async () => {
+
+            try{
+            const response = await axios.get('https://fakestoreapi.com/products');
+            setData(response.data);
+            }catch(error){
+                console.log(error);
+            }
+
+    };
+
+
+    useEffect(()=>{
+        fetchData();
+    })
+
+
+  return (
+    <div>
+        {
+           data.map((ele,index)=>{
+                return <Product  title={ele.title} price={ele.price} desc={ele.category} image={ele.image} />
+           })
+        }
+    </div>
+  )
+
 }
